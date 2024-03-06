739d501e6ef971cb8fa34268ce7cba8512cdd7ff
Asier Cidon <asier.cidon@gmail.com>
2024-03-06 12:16:05 +0100

    Added things

--- 01-clean.sh
+++ 01-clean.sh
@@ -3,3 +3,5 @@ oc delete dr egressgateway-for-nginx -n user1
 oc delete gw istio-egressgateway -n user1
 oc delete secret -n istio-system user1-mesh-external-egress-nginx-client-certs
 oc delete dr -n istio-system  originate-mtls-for-nginx-user1
+oc delete secret -n istio-system user2-mesh-external-egress-nginx-client-certs
+oc delete dr -n istio-system  originate-mtls-for-nginx-user2
