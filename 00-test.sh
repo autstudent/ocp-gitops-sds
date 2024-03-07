066ec5b37d8b518372a22b21e87b65419038591e
Asier Cidon <asier.cidon@gmail.com>
2024-03-07 12:37:51 +0100

    Added service entries

--- 00-test.sh
+++ 00-test.sh
@@ -10,3 +10,5 @@ oc exec -it $POD -c sleep -- curl -v http://my-nginx.user1-mesh-external.svc.clu
 oc exec -it $POD -c sleep -- curl -v http://my-nginx-es.user1-mesh-external.svc.cluster.local -k
 oc exec -it $POD -c sleep -- curl -v http://my-nginx-mx.user1-mesh-external.svc.cluster.local -k
 
+POD=$(oc get po -l app=sleep -o jsonpath='{.items[0].metadata.name}')
+oc exec -it $POD -c sleep -- curl -v http://my-nginx-mx.user1-mesh-external.svc.cluster.local -k
